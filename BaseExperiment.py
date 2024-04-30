d4de3d1acaee440d2095020354bbb17588293a75
Thiago Matheus <thiago.matheus.bruno@gmail.com>
2024-04-30 20:51:14 -0300

    Update

--- BaseExperiment.py
+++ BaseExperiment.py
@@ -305,8 +305,8 @@ def test_model(testloader, training_config):
             if training_config['in_shape'][0] == 1:
                 inputs = inputs.permute(1, 0, 2, 3)
             
-            # print('debug')
-            # print(inputs.shape)
+            print('debug')
+            print(inputs.shape)
             # inputs = inputs.unsqueeze(1)
             y_pred_s, _ = model(inputs.to(device))
             y_pred = y_pred_s
